e3f223602f7805620f26bdc06d2de6c86e28d807
Dane Evans <dane@2pisoftware.com>
2023-01-19 19:17:49 +1100

    fix linter script

--- helpers/linter.py
+++ helpers/linter.py
@@ -9,27 +9,23 @@ base_url = "https://centre-for-invasive-species-solutions.github.io/demo_json_ap
 
 def lint_repo():
         print(os.getcwd())
-
+        ret = 0 
         for current_dir, subdirs, files in os.walk( data_source ):
-            # Reletive directory 
+            # Reletive directory
             write_dir = current_dir[len(data_source)+1:]
-            ret = 0 
-
             # Files
             for filename in files:
                 if filename.lower().endswith('json'):
-                    out_file = filename.strip('json')
-                    out_file += ("md")
                     relative_path = os.path.join( current_dir, filename )
                     with open(relative_path) as json_file:
                         try:
-                            data = json.load(json_file) 
-                            print(data.keys())
+                            data = json.load(json_file)
                         except Exception as exception:
-                            print("   json parse failure:" +write_dir+"/   "+ filename)
+                            print("   json parse failure:  " +write_dir+"/"+ filename)
                             print(exception)
                             ret = 1
-            return ret
+        if ret: 
+            exit(1)
   
 
 if __name__=="__main__":
